7eeffc27ec2ad04abe6c4ae03d802c9a2009e47e
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-05-24 11:19:56 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -9,10 +9,24 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// connection with mongodb
 
-
-app.get("/", async (req, res) => {
-  res.send("working");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hoyf4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
+
+const run = async () => {
+  await client.connect();
+  const productsCollection = await client.db("tools").collection("products");
+
+  app.get("/", async (req, res) => {
+    res.send("working");
+  });
+};
+
+run().catch(console.dir);
 
 app.listen(port, () => console.log("running at ", port));
